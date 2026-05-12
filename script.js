6770c955d46dc86d221a47f67474cd5c5b991469
Karl Davis <karld809@gmail.com>
2026-05-12 15:00:01 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+function toggleFaq (A1, plus1) {
+    const answer = document.getElementById("A1");
+    answer.classList.toggle("hidden");
+
+    const imgElement = document.getElementById("plus1");
+    const plusIcon = "assets/images/icon-plus.svg";
+    const minusIcon = "assets/images/icon-minus.svg";
+
+    if (imgElement.src.includes("assets/images/icon-plus.svg")) {
+        imgElement.src = minusIcon;
+    } else {
+        imgElement.src = plusIcon;
+    }
+}
+function toggleFaq2 (A2, plus2) {
+    const answer = document.getElementById("A2");
+    answer.classList.toggle("hidden");
+
+    const imgElement = document.getElementById("plus2");
+    const plusIcon = "assets/images/icon-plus.svg";
+    const minusIcon = "assets/images/icon-minus.svg";
+
+    if (imgElement.src.includes("assets/images/icon-plus.svg")) {
+        imgElement.src = minusIcon;
+    } else {
+        imgElement.src = plusIcon;
+    }
+}
+function toggleFaq3 (A3, plus3) {
+    const answer = document.getElementById("A3");
+    answer.classList.toggle("hidden");
+
+    const imgElement = document.getElementById("plus3");
+    const plusIcon = "assets/images/icon-plus.svg";
+    const minusIcon = "assets/images/icon-minus.svg";
+
+    if (imgElement.src.includes("assets/images/icon-plus.svg")) {
+        imgElement.src = minusIcon;
+    } else {
+        imgElement.src = plusIcon;
+    }
+}
+function toggleFaq4 (A4, plus4) {
+    const answer = document.getElementById("A4");
+    answer.classList.toggle("hidden");
+
+    const imgElement = document.getElementById("plus4");
+    const plusIcon = "assets/images/icon-plus.svg";
+    const minusIcon = "assets/images/icon-minus.svg";
+
+    if (imgElement.src.includes("assets/images/icon-plus.svg")) {
+        imgElement.src = minusIcon;
+    } else {
+        imgElement.src = plusIcon;
+    }
+}
